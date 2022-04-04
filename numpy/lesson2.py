c87ff4ebb9915c92486cbc2b9b9fc6d6675e51d6
Constantin Shkutkov <shkutkoff@gmail.com>
2022-04-04 19:03:12 +0300

    04.04.2022

---
+++ numpy/lesson2.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+a = np.array([1, 2, 3, 4])
+print(a)
+b = np.array([1, 2, 3, 4], 'float64')
+print(b)
+
+print(np.complex64(10))
+
+print(np.int16(10.5))
+
+print(np.complex64(a))
+
+zxc = np.array([[1, 2], [3, 4], [5, 6]])
+print(zxc)
