a595e8bb4a7090c8c40208b98a63eb34bf59965d
Constantin Shkutkov <shkutkoff@gmail.com>
2022-04-07 21:55:33 +0300

    07.04.2022

---
+++ numpy/lesson8.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+lst = [1, 2, 3]
+
+a = np.array([1, 2, 3])
+print(lst * 2)
+print(a * 2)
+print(-a)
+print(a - 3)
+print(a * 5)
+print(a // 2)
+print(a ** 2)
+# elem + elem(*-/)
+b = np.array([3, 4, 5])
+print(a - b)
+print(a + b)
+# translation arrays
+b = np.arange(1, 7)
+b.resize(2, 3)
+print(a + b)
+print(a * b)
+print(a - b)
+print( a+= 5) # update a
+print( a + 5) # coppy new array
